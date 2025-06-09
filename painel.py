f74147dc3160f596b925ae1c4c9d9c1b34c15fd4
Piokamunga <willboyhp@gmail.com>
2025-06-09 17:32:01 +0100

    Update painel.py
    git add painel.py
    git commit -m "Implementa servidor FastAPI para exibir sinais e gráfico em tempo real"

--- painel.py
+++ painel.py
@@ -3,22 +3,26 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import json
+import os
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
-async def painel(request: Request):
+async def exibir_painel(request: Request):
+    sinais = []
     try:
-        with open("sinais.json") as f:
-            sinais = json.load(f)[-20:]
+        with open("logs/sinais.jsonl", "r", encoding="utf-8") as f:
+            for linha in f.readlines()[-20:]:
+                sinais.append(json.loads(linha))
     except:
-        sinais = []
-    return templates.TemplateResponse("painel.html", {"request": request, "sinais": sinais})
-
-async def iniciar_painel():
-    import uvicorn
-    config = uvicorn.Config("painel:app", host="0.0.0.0", port=8080, reload=False)
-    server = uvicorn.Server(config)
-    await server.serve()
+        pass
+    sinais.reverse()
+    return templates.TemplateResponse("painel.html", {
+        "request": request,
+        "sinais": sinais,
+        "chart_url": "/static/chart.png",
+        "banner_link": "https://bit.ly/449TH4F",
+        "banner_img": "https://i.ibb.co/ZcK9dcT/banner.png"
+    })
