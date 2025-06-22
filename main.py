708de494a45b751f64e8576f1b59af627848c44a
Piokamunga <willboyhp@gmail.com>
2025-06-22 18:39:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import asyncio
 from aiohttp import web
-from telegrambotpy.py import iniciar_scraping
-
+from telegrambotpy import iniciar_scraping
 async def iniciar_servidor():
     async def handle(request):
         return web.Response(text="Bot Aviator rodando 24/7 no Render! 🚀")
